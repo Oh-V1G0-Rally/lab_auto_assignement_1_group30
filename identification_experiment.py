8b7e17dae457b67be68a4d5e4a226e6a046d1993
Oh-V1G0-Rally <76070738+Oh-V1G0-Rally@users.noreply.github.com>
2026-04-03 14:55:00 +0200

    update with matlab test

--- identification_experiment.py
+++ identification_experiment.py
@@ -36,13 +36,13 @@ dof = robot.get_input_number()
 Tc = robot.get_sampling_period()
 
 # define chirp
-Duration = 60.0 # seconds
+Duration = 70.0 # seconds
 t = np.arange(0, Duration + Tc, Tc)  # Ensure inclusion of Duration if possible
 
 f0=0.1
 f1=500.0 # Tc=0.001 Fc=1000Hz, Shannon/Nyquist 500Hz
 A= 40.0
-joint_number=0  # array index
+joint_number=2  # array index
 chirp_signal = A*chirp(t, f0=f0, f1=f1, t1=Duration, method='logarithmic')
 
 
