cc79fcdc84d393aba56fdca6ab32a489093ec123
Oh-V1G0-Rally <76070738+Oh-V1G0-Rally@users.noreply.github.com>
2026-04-28 16:07:35 +0200

    Remake J3 model and tuned

--- identification_experiment.py
+++ identification_experiment.py
@@ -41,8 +41,8 @@ t = np.arange(0, Duration + Tc, Tc)  # Ensure inclusion of Duration if possible
 
 f0=0.1
 f1=300.0 # Tc=0.001 Fc=1000Hz, Shannon/Nyquist 500Hz
-A= 10.0
-joint_number=2  # array index
+A= 30.0
+joint_number=0  # array index
 chirp_signal = A*chirp(t, f0=f0, f1=f1, t1=Duration, method='logarithmic')
 
 
